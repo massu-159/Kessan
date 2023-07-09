815816771984ea2c154a172dea2fbb32e260148d
massu-159 <magjtwjp@gmail.com>
2023-07-09 09:07:06 +0900

    use appDir

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    serverActions: true,
+    appDir: true,
+  },
+  images: {
+    domains: ['plgmtpwjxodyofnrutcn.supabase.co'],
+  },
 }
 
 module.exports = nextConfig
