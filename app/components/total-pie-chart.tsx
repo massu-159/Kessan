9ca5a8bc22db3a1ba2833a1260fd9b0c6508e5b5
massu-159 <magjtwjp@gmail.com>
2023-07-12 22:54:35 +0900

    create barchart piechart

---
+++ app/components/total-pie-chart.tsx
@@ -0,0 +1,37 @@
+'use client'
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts'
+
+const data = [
+  { name: 'Group A', value: 400 },
+  { name: 'Group B', value: 300 },
+  { name: 'Group C', value: 300 },
+  { name: 'Group D', value: 200 },
+]
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042']
+
+const TotalPieChart = () => {
+  return (
+    <ResponsiveContainer width='100%' height='100%'>
+      <PieChart>
+        <Pie
+          width={240}
+          height={240}
+          data={data}
+          cx={120}
+          cy={200}
+          innerRadius={80}
+          outerRadius={120}
+          fill='#8884d8'
+          paddingAngle={1}
+          dataKey='value'
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+      </PieChart>
+    </ResponsiveContainer>
+  )
+}
+
+export default TotalPieChart
