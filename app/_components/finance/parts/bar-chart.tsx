a290775c3b919f62a87c85b44fcfabc0f60bb486
massu-159 <magjtwjp@gmail.com>
2024-07-02 20:18:00 +0900

    各資産棒グラフの凡例変更

--- app/_components/finance/parts/bar-chart.tsx
+++ app/_components/finance/parts/bar-chart.tsx
@@ -28,6 +28,19 @@ const FinanceBarChart = ({ data, index }: Props) => {
     a.date && b.date ? a.date?.localeCompare(b.date) : 0
   )
 
+  // 日本円のフォーマット
+  const currencyFormatter = (value: number) => [
+    `${value.toLocaleString()}円`, '残高'
+  ]
+
+  // 日付のフォーマット
+  const DateFormatter = (dateString: string) => {
+    const date = new Date(dateString)
+    const year = date.getFullYear()
+    const month = ('0' + (date.getMonth() + 1)).slice(-2)
+    return `${year}/${month}`
+  }
+
   return (
     <ResponsiveContainer width='100%' height='100%'>
       <ComposedChart
@@ -37,19 +50,25 @@ const FinanceBarChart = ({ data, index }: Props) => {
         margin={{
           top: 20,
           right: 30,
-          left: 20,
+          left: 50,
           bottom: 5,
         }}
       >
         <CartesianGrid strokeDasharray='3 3' />
-        <XAxis dataKey='date' tick={{ fill: '#fff' }} />
-        <YAxis tick={{ fill: '#fff' }} />
-        <Tooltip />
+        <XAxis
+          dataKey='date'
+          tick={{ fill: '#fff' }}
+          tickFormatter={DateFormatter}
+        />
+        <YAxis
+          tick={{ fill: '#fff' }}
+          tickFormatter={(value) => `${value.toLocaleString()}円`}
+        />
+        <Tooltip formatter={currencyFormatter} />
         <Bar
           dataKey='amount'
           stackId='a'
           fill={colors[colorIndex]}
-          unit='円'
           barSize={28}
         />
       </ComposedChart>
