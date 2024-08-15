e3e191f73b931ce0ecfd86edd340f2e2e527fb34
massu-159 <magjtwjp@gmail.com>
2024-08-15 20:18:00 +0900

    update user info handling

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,27 +1,23 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-import { Database } from '../../lib/database.types'
-import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 import LoginPage from './(auth)/login/page'
 import DashboardPage from './(dashboard)/dashboard/page'
 import { dashboardPath } from '../_common/constants/path'
+import { createClient } from '../../utils/supabase/server'
 
 const Home = async () => {
-  const supabase = createServerComponentClient<Database>({
-    cookies,
-  })
+  const supabase = createClient()
 
   // セッションの取得
   const {
-    data: { session },
-  } = await supabase.auth.getSession()
+    data: { user },
+  } = await supabase.auth.getUser()
 
   // 認証している場合、リダイレクト
-  if (session) {
+  if (user) {
     redirect(dashboardPath)
   }
 
-  return <div>{session ? <DashboardPage /> : <LoginPage />}</div>
+  return <div>{user ? <DashboardPage /> : <LoginPage />}</div>
 }
 
 export default Home
