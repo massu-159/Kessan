8ac41be96c0d79f32b18cb31c6053035ef1715fe
massu-159 <magjtwjp@gmail.com>
2024-08-23 20:18:00 +0900

    update API get goal

--- app/_components/total-assets/sections/asset-actual-vs-target.tsx
+++ app/_components/total-assets/sections/asset-actual-vs-target.tsx
@@ -1,32 +1,53 @@
 import { CardBody, CardFooter, Typography } from '../../common'
 import { CurrencyYenIcon } from '@heroicons/react/24/solid'
 import GoalEditButton from '../parts/goal-edit-button'
-import { AmountRecord } from '../../../_common/types/AmountRecord'
 import { CustomCard } from '../../ui/custom-card'
 import { Suspense } from 'react'
 import Loading from '../../../(routes)/loading'
-
-type Props = {
-  goal: any
-  record: AmountRecord | null
-  userId: string | undefined
-}
+import { createClient } from '../../../../utils/supabase/server'
+import { getGoal } from '../../../api/goal/fetcher'
+import { getAssetPerDate } from '../../../api/asset/fetcher'
+import {
+  calcDifference,
+  calcTargetAchievementRate,
+  calcTotalAmountParDate,
+} from '../../../_common/utils/calc'
+import { redirect } from 'next/navigation'
 
 /**
  * 実績と目標の比較カード
- * @param goal 目標
- * @param record 実績
- * @param userId ユーザーID
  */
-const AssetActualVsTarget = ({ goal, record, userId }: Props) => {
-  let difference = 0
-  let rate = 0
-  if (goal && record && goal.amount && record.amount) {
-    // 目標と実績の差分
-    difference = goal.amount - record.amount
-    // 目標達成率
-    rate = Math.round((record.amount / goal.amount) * 100)
+const AssetActualVsTarget = async () => {
+  const supabase = createClient()
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+  const userId = user?.id
+  // ユーザーが存在しない場合はログイン画面にリダイレクト
+  if (!user || !userId) {
+    return redirect('/login')
   }
+
+  // 資産を取得
+  const assetParDate = await getAssetPerDate(userId)
+  // 各日付ごとの資産合計を計算
+  const totalAmountParDate = calcTotalAmountParDate(assetParDate)
+
+  // 目標を取得
+  const goal = await getGoal(userId)
+  if (!goal || !totalAmountParDate[0]) {
+    // TODO: 目標未設定のコンポーネントを作る
+    return <Loading />
+  }
+
+  // 目標と実績の差分
+  const difference = calcDifference(goal.amount, totalAmountParDate[0].amount)
+  // 目標達成率
+  const rate = calcTargetAchievementRate(
+    goal.amount,
+    totalAmountParDate[0].amount
+  )
+
   return (
     <CustomCard className='bg-opacity-0 text-white'>
       <Suspense fallback={<Loading />}>
@@ -50,11 +71,11 @@ const AssetActualVsTarget = ({ goal, record, userId }: Props) => {
               fill='#00C49F'
               className='w-10 h-10'
             ></CurrencyYenIcon>
-            <GoalEditButton goal={goal} userId={userId}></GoalEditButton>
+            <GoalEditButton goal={goal}></GoalEditButton>
           </div>
 
           <div className='flex justify-center items-center gap-6'>
-            <div>{record?.date} 現在</div>
+            <div>{totalAmountParDate[0]?.date} 現在</div>
             <Typography
               variant='lead'
               className='text-blue-100 font-bold flex justify-center items-center'
