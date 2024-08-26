ccb6cf4c698d5bbb9732bf8450249fb0b43ca4ba
massu-159 <magjtwjp@gmail.com>
2024-08-26 20:18:00 +0900

    update bar-chart

--- app/_components/total-assets/sections/asset-total-bar-chart-card.tsx
+++ app/_components/total-assets/sections/asset-total-bar-chart-card.tsx
@@ -1,26 +1,47 @@
 import { Suspense } from 'react'
-import { ProcessedData } from '../../../_common/types/ProcessedData'
 import { CardBody } from '../../common'
 import { CustomCard } from '../../ui/custom-card'
 import AssetTotalBarChart from '../parts/asset-total-bar-chart'
 import Loading from '../../../(routes)/loading'
-
-type Props = {
-  data: ProcessedData[]
-}
+import { createClient } from '../../../../utils/supabase/server'
+import { redirect } from 'next/navigation'
+import { getAssetPerDate } from '../../../api/asset/fetcher'
+import { calcEachAssetPerDate } from '../../../_common/utils/calc'
+import AssetNoDataBarChart from './asset-no-data-bar-chart'
 
 /**
  * 資産棒グラフ
  */
-const AssetTotalBarChartCard = ({ data }: Props) => {
+const AssetTotalBarChartCard = async () => {
+  const supabase = createClient()
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+  const userId = user?.id
+  // ユーザーが存在しない場合はログイン画面にリダイレクト
+  if (!user || !userId) {
+    return redirect('/login')
+  }
+
+  // 資産を取得
+  const assetParDate = await getAssetPerDate(userId)
+  // 資産を日付ごとに集計
+  const eachAssetPerDate = calcEachAssetPerDate(assetParDate)
+
   return (
-    <CustomCard>
-      <CardBody className='w-11/12 h-96 mx-auto'>
-        <Suspense fallback={<Loading />}>
-          <AssetTotalBarChart data={data}></AssetTotalBarChart>
-        </Suspense>
-      </CardBody>
-    </CustomCard>
+    <>
+      {eachAssetPerDate.length > 1 ? (
+        <CustomCard>
+          <CardBody className='w-11/12 h-96 mx-auto'>
+            <Suspense fallback={<Loading />}>
+              <AssetTotalBarChart data={eachAssetPerDate}></AssetTotalBarChart>
+            </Suspense>
+          </CardBody>
+        </CustomCard>
+      ) : (
+        <AssetNoDataBarChart />
+      )}
+    </>
   )
 }
 
