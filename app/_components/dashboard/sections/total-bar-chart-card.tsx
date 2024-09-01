1b8210831d1db720d54e38418fba8af50fe66d7d
massu-159 <magjtwjp@gmail.com>
2024-09-01 20:18:00 +0900

    update bar-chart

--- app/_components/dashboard/sections/total-bar-chart-card.tsx
+++ app/_components/dashboard/sections/total-bar-chart-card.tsx
@@ -3,36 +3,56 @@ import { totalAssetsPath } from '../../../_common/constants/path'
 import { Button, CardBody, CardFooter } from '../../common'
 import { CustomCard } from '../../ui/custom-card'
 import TotalBarChart from '../parts/total-bar-chart'
-import { ProcessedData } from '../../../_common/types/ProcessedData'
 import { Suspense } from 'react'
 import Loading from '../../../(routes)/loading'
-
-type Props = {
-  accountData: ProcessedData[]
-}
+import { createClient } from '../../../../utils/supabase/server'
+import { redirect } from 'next/navigation'
+import { getAssetPerDate } from '../../../api/asset/fetcher'
+import { calcEachAssetPerDate } from '../../../_common/utils/calc'
+import BarChartNoDataCard from './bar-chart-no-data-card'
 
 /**
  * 棒グラフカード
  * @param accountData 資産データ
  */
-const TotalBarChartCard = ({ accountData }: Props) => {
+const TotalBarChartCard = async () => {
+  const supabase = createClient()
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+  const userId = user?.id
+  // ユーザーが存在しない場合はログイン画面にリダイレクト
+  if (!user || !userId) {
+    return redirect('/login')
+  }
+
+  // 資産を取得
+  const assetParDate = await getAssetPerDate(userId)
+  // 資産を日付ごとに集計
+  const eachAssetPerDate = calcEachAssetPerDate(assetParDate)
   return (
-    <div>
-      <CustomCard>
-        <Suspense fallback={<Loading />}>
-          <CardBody className='w-11/12 h-96 mx-auto'>
-            <TotalBarChart data={accountData}></TotalBarChart>
-          </CardBody>
-          <CardFooter className='pt-0 text-end'>
-            <Link href={totalAssetsPath}>
-              <Button color='cyan' variant='text'>
-                overview →
-              </Button>
-            </Link>
-          </CardFooter>
-        </Suspense>
-      </CustomCard>
-    </div>
+    <>
+      {eachAssetPerDate.length > 1 ? (
+        <div>
+          <CustomCard>
+            <Suspense fallback={<Loading />}>
+              <CardBody className='w-11/12 h-96 mx-auto'>
+                <TotalBarChart data={eachAssetPerDate}></TotalBarChart>
+              </CardBody>
+              <CardFooter className='pt-0 text-end'>
+                <Link href={totalAssetsPath}>
+                  <Button color='cyan' variant='text'>
+                    overview →
+                  </Button>
+                </Link>
+              </CardFooter>
+            </Suspense>
+          </CustomCard>
+        </div>
+      ) : (
+        <BarChartNoDataCard />
+      )}
+    </>
   )
 }
 
