6772057443969b77d28d40426507d33466aa4d86
massu-159 <magjtwjp@gmail.com>
2024-09-02 20:18:00 +0900

    update pie-chart

--- app/_components/dashboard/sections/total-pie-chart-card.tsx
+++ app/_components/dashboard/sections/total-pie-chart-card.tsx
@@ -2,35 +2,54 @@ import Link from 'next/link'
 import { totalAssetsPath } from '../../../_common/constants/path'
 import { Button, CardBody, CardFooter } from '../../common'
 import { CustomCard } from '../../ui/custom-card'
-import { ProcessedData } from '../../../_common/types/ProcessedData'
 import TotalPieChart from '../parts/total-pie-chart'
 import { Suspense } from 'react'
 import Loading from '../../../(routes)/loading'
-
-type Props = {
-  accountData: ProcessedData[]
-}
+import { calcEachAssetPerDate } from '../../../_common/utils/calc'
+import { getAssetPerDate } from '../../../api/asset/fetcher'
+import { redirect } from 'next/navigation'
+import { createClient } from '../../../../utils/supabase/server'
+import PieChartNoDataCard from './pie-chart-no-data-card'
 
 /**
- * 棒グラフカード
- * @param accountData 資産データ
+ * 円グラフカード
  */
-const TotalPieChartCard = ({ accountData }: Props) => {
+const TotalPieChartCard = async () => {
+  const supabase = createClient()
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+  const userId = user?.id
+  // ユーザーが存在しない場合はログイン画面にリダイレクト
+  if (!user || !userId) {
+    return redirect('/login')
+  }
+
+  // 資産を取得
+  const assetParDate = await getAssetPerDate(userId)
+  // 資産を日付ごとに集計
+  const eachAssetPerDate = calcEachAssetPerDate(assetParDate)
   return (
-    <CustomCard className='bg-opacity-0 shadow-none'>
-      <Suspense fallback={<Loading />}>
-        <CardBody className='w-full h-96 flex justify-center items-center'>
-          <TotalPieChart data={accountData[0]}></TotalPieChart>
-        </CardBody>
-        <CardFooter className='pt-0 text-end'>
-          <Link href={totalAssetsPath}>
-            <Button color='white' variant='text'>
-              overview →
-            </Button>
-          </Link>
-        </CardFooter>
-      </Suspense>
-    </CustomCard>
+    <>
+      {eachAssetPerDate.length > 1 ? (
+        <CustomCard className='bg-opacity-0 shadow-none'>
+          <Suspense fallback={<Loading />}>
+            <CardBody className='w-full h-96 flex justify-center items-center'>
+              <TotalPieChart data={eachAssetPerDate[0]}></TotalPieChart>
+            </CardBody>
+            <CardFooter className='pt-0 text-end'>
+              <Link href={totalAssetsPath}>
+                <Button color='white' variant='text'>
+                  overview →
+                </Button>
+              </Link>
+            </CardFooter>
+          </Suspense>
+        </CustomCard>
+      ) : (
+        <PieChartNoDataCard />
+      )}
+    </>
   )
 }
 
